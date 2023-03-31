16a6dad10ac761644a9bf7e5467dbb2c50db2b02
ibaysinger <54559378+ibaysinger@users.noreply.github.com>
2023-03-31 17:10:26 -0500

    Minor Tweaks to Walking

--- Assets/Scripts/WalkInPlaceLocomotion.cs
+++ Assets/Scripts/WalkInPlaceLocomotion.cs
@@ -14,9 +14,12 @@ public class WalkInPlaceLocomotion : MonoBehaviour
 
     private Vector3 gravity = new Vector3(0, -9.8f, 0);
 
-    [SerializeField] private float speed = 4;
-    [SerializeField] private float velocityTriggerAmount = 0.05f;
+    [SerializeField] private float speed = 1;
+    [SerializeField] private float velocityWalkTriggerAmount = 0.008f;
+    [SerializeField] private float velocityRunTriggerAmount = 0.04f;
+    [SerializeField] private float runTimeInterval = 0.75f;
 
+    private float timeSinceLastSwing = 0;
     bool hasSwungOnce = false;
 
     public InputActionReference toggleReferenceRight = null;
@@ -24,6 +27,10 @@ public class WalkInPlaceLocomotion : MonoBehaviour
 
     private bool leftTriggerPressed;
     private bool rightTriggerPressed;
+
+    //Sounds for footsteps
+    public AudioClip[] footsteps;
+    public AudioSource footstepsSource;
 
     private void Awake()
     {
@@ -51,16 +58,25 @@ public class WalkInPlaceLocomotion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Calculate the velocity of the player hand movement
-        Vector3 leftHandVelocity = leftHand.transform.position - previousPosLeft;
-        Vector3 rightHandVelocity = rightHand.transform.position - previousPosRight;
-        float totalVelocity =+ leftHandVelocity.magnitude * 0.8f +rightHandVelocity.magnitude * 0.8f;
-
         if (leftTriggerPressed && rightTriggerPressed)
         {
-            if (totalVelocity >= velocityTriggerAmount) //If true, player has swung their hands
-            {
+            //Calculate the velocity of the player hand movement
+            Vector3 leftHandVelocity = leftHand.transform.position - previousPosLeft;
+            Vector3 rightHandVelocity = rightHand.transform.position - previousPosRight;
+            float totalVelocity = +leftHandVelocity.magnitude * 0.8f + rightHandVelocity.magnitude * 0.8f;
+            Debug.Log(totalVelocity);
 
+            if (totalVelocity >= velocityRunTriggerAmount) //If true, player has swung their hands
+            {
+                //Debug.Log("Running!");
+                //getting the direction that the player is facing
+                direction = Camera.main.transform.forward;
+
+                //move the player using the character controller
+                characterController.Move((speed * 3) * Time.deltaTime * Vector3.ProjectOnPlane(direction, Vector3.up));
+            } else if (totalVelocity >= velocityWalkTriggerAmount)
+            {
+                //Debug.Log("Walking!");
                 //getting the direction that the player is facing
                 direction = Camera.main.transform.forward;
 
